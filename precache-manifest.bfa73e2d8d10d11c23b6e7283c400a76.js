c1d0d1c143a466a3f717261b0e58e8fccc560dbc
gcali <gcali.git@gmailcom>
2020-10-24 23:24:19 +0200

    2ce05de

---
+++ precache-manifest.bfa73e2d8d10d11c23b6e7283c400a76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0941681df1a03251815e",
+    "url": "/duetto/css/app.955b4ea2.css"
+  },
+  {
+    "revision": "e057641b57bd0ddeeb10ebf1a8686e23",
+    "url": "/duetto/index.html"
+  },
+  {
+    "revision": "0941681df1a03251815e",
+    "url": "/duetto/js/app.03b2a401.js"
+  },
+  {
+    "revision": "b82cdf3ca6f497a39134",
+    "url": "/duetto/js/chunk-vendors.a3a92282.js"
+  },
+  {
+    "revision": "a838d4e31d69e2d36645afea42626879",
+    "url": "/duetto/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/duetto/robots.txt"
+  }
+]);
